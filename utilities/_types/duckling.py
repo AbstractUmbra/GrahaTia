0e4e30935215414533e1811bf9139141a64cf467
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2023-07-12 10:31:18 +0100

    black and isort

--- utilities/_types/duckling.py
+++ utilities/_types/duckling.py
@@ -4,9 +4,11 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 
-from typing import Literal, TypedDict
+from typing import TYPE_CHECKING, Literal, TypedDict
 
-from typing_extensions import NotRequired
+
+if TYPE_CHECKING:
+    from typing_extensions import NotRequired
 
 
 __all__ = ("DucklingResponse",)
