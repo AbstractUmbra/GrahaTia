3777efa3ac07f2dd3e3101c4a7c38e51390d38c9
Alex Nørgaard <umbra@abstractumbra.dev>
2024-05-01 14:20:44 +0100

    needless type ignore fixed

--- extensions/stats.py
+++ extensions/stats.py
@@ -263,7 +263,7 @@ class Stats(BaseCog["Graha"]):
 
     def get_last_commits(self, count: int = 3) -> str:
         repo = pygit2.Repository(".git")
-        commits = list(itertools.islice(repo.walk(repo.head.target, pygit2.enums.SortMode.TOPOLOGICAL), count))  # type: ignore # pygit doesnt expose the enums
+        commits = list(itertools.islice(repo.walk(repo.head.target, pygit2.enums.SortMode.TOPOLOGICAL), count))
         return "\n".join(self.format_commit(c) for c in commits)
 
     @commands.command()
