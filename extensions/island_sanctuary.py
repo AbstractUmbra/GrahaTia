0f6e1e3633f5036fd333917b2926499be65c0f67
Alex Nørgaard <umbra@abstractumbra.dev>
2025-06-03 13:51:41 +0100

    include overseas casuals stuff

--- extensions/island_sanctuary.py
+++ extensions/island_sanctuary.py
@@ -11,6 +11,9 @@ from utilities.shared.time import Weekday, resolve_next_weekday
 if TYPE_CHECKING:
     from bot import Graha
 
+OC_INVITE: str = "https://discord.gg/overseascasuals"
+OC_BOT_CHANNEL: str = "https://canary.discord.com/channels/1034534280757522442/1034985297391407126"
+
 
 class IslandSanctuary(BaseCog["Graha"]):
     def next_reset(self, *, source: datetime.datetime | None = None) -> datetime.datetime:
@@ -19,9 +22,16 @@ class IslandSanctuary(BaseCog["Graha"]):
         )
 
     def generate_embed(self, *, when: datetime.datetime | None = None) -> discord.Embed:
-        embed = discord.Embed(title="Island Sanctuary workshop has reset!")
+        embed = discord.Embed(title="Island Sanctuary workshop has reset!", url=OC_INVITE)
         embed.set_image(url="https://static.abstractumbra.dev/images/sanctuary.png")
+        embed.description = (
+            "The link in this embed is a Discord invite to the Overseas Casuals server.\n"
+            "They optimize and automate reporting on which island workshop crafting schedule will maximize your profile! "
+            "Check them out and after finding your way around, check out the "
+            f"[#bot-spam]({OC_BOT_CHANNEL} channel and use their bot to get your weekly plannings!)"
+        )
         embed.timestamp = self.next_reset(source=when)
+        embed.set_footer(text="Not affiliated with OC in any way, just a fan!")
 
         return embed
 
