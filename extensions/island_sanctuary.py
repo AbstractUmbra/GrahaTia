34132cc3d9dd38efc20b8a06b2cfec0d8503dfd5
Alex Nørgaard <umbra@abstractumbra.dev>
2025-07-06 13:37:27 +0100

    correct colouring

--- extensions/island_sanctuary.py
+++ extensions/island_sanctuary.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 import discord
 
 from utilities.shared.cog import BaseCog
+from utilities.shared.formats import random_pastel_colour
 from utilities.shared.time import Weekday, resolve_next_weekday
 
 if TYPE_CHECKING:
@@ -18,17 +19,18 @@ OC_BOT_CHANNEL: str = "https://canary.discord.com/channels/1034534280757522442/1
 class IslandSanctuary(BaseCog["Graha"]):
     def next_reset(self, *, source: datetime.datetime | None = None) -> datetime.datetime:
         return resolve_next_weekday(
-            target=Weekday.monday, source=source, current_week_included=True, before_time=datetime.time(hour=8)
+            target=Weekday.tuesday, source=source, current_week_included=True, before_time=datetime.time(hour=8)
         )
 
     def generate_embed(self, *, when: datetime.datetime | None = None) -> discord.Embed:
-        embed = discord.Embed(title="Island Sanctuary workshop has reset!", url=OC_INVITE)
+        when = when or (datetime.datetime.now(datetime.UTC) - datetime.timedelta(minutes=15))
+        embed = discord.Embed(title="Island Sanctuary workshop has reset!", url=OC_INVITE, colour=random_pastel_colour())
         embed.set_image(url="https://static.abstractumbra.dev/images/sanctuary.png")
         embed.description = (
             "The link in this embed is a Discord invite to the Overseas Casuals server.\n"
             "They optimize and automate reporting on which island workshop crafting schedule will maximize your profile! "
             "Check them out and after finding your way around, check out the "
-            f"[#bot-spam]({OC_BOT_CHANNEL} channel and use their bot to get your weekly plannings!)"
+            f"[#bot-spam]({OC_BOT_CHANNEL}) channel and use their bot to get your weekly plannings!"
         )
         embed.timestamp = self.next_reset(source=when)
         embed.set_footer(text="Not affiliated with OC in any way, just a fan!")
